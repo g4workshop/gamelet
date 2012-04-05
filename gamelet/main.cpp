3973366086224bdd8c8a9aebd5a302126421c624
dourgulf <g4workshop@foxmail.com>
2012-04-05 17:08:53 +0800

    Initial Commit

---
+++ gamelet/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gamelet
+//
+//  Created by Dawen Rie on 12-4-5.
+//  Copyright (c) 2012年 G4 Workshop. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
